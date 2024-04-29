15ea97c6d7743fa7e535fd38eb796f3a3842209a
pyaesoneag <pyaesoneaung.psa1991@gmail.com>
2024-04-29 10:30:23 +0700

    test16

--- test15.py
+++ test15.py
@@ -2,13 +2,22 @@
 
 myfav_artists = ['chris brown', 'lyn lyn', 'sean kingston', 'florida']
 
-print(type(myfav_artists))
+# print(type(myfav_artists))
+
+myfav_artists[3] = 'chainsmoker'
+print(myfav_artists)
+
+# myfav_artists.sort()
+# print(myfav_artists)
+
+# myfav_artists.insert(0, 'Lout Sa Lone')
+# print(myfav_artists)
 
 # print(myfav_artists)
 # print(myfav_artists[0])
 # print(len(myfav_artists))
 
-print(myfav_artists[2])
-print(myfav_artists[1:4])
-print(myfav_artists[1:])
-print(myfav_artists[:4])
+# print(myfav_artists[2])
+# print(myfav_artists[1:4])
+# print(myfav_artists[1:])
+# print(myfav_artists[:4])
---
+++ test16.py
@@ -0,0 +1,8 @@
+artist_lyn_lyn = {
+'name' : 'lyn_lyn' ,
+'genre' : 'rock'}
+
+print(type(artist_lyn_lyn))
+print(artist_lyn_lyn['name'])
+artist_lyn_lyn["popular_song"] = "Yaung Ni"
+print(artist_lyn_lyn)
