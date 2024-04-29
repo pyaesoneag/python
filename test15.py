0c9bf62f64994dc87cdacefd9309628d798d9b6d
pyaesoneag <pyaesoneaung.psa1991@gmail.com>
2024-04-29 09:53:02 +0700

    test15

---
+++ test15.py
@@ -0,0 +1,14 @@
+#List
+
+myfav_artists = ['chris brown', 'lyn lyn', 'sean kingston', 'florida']
+
+print(type(myfav_artists))
+
+# print(myfav_artists)
+# print(myfav_artists[0])
+# print(len(myfav_artists))
+
+print(myfav_artists[2])
+print(myfav_artists[1:4])
+print(myfav_artists[1:])
+print(myfav_artists[:4])
